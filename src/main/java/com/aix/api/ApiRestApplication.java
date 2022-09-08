6292954cff38ff26a5d659628f625df0e77bf95d
José Luis Pacheco <jpacheco@hightelecom.com>
2022-09-08 09:12:53 -0500

    3 commit

--- src/main/java/com/aix/api/ApiRestApplication.java
+++ src/main/java/com/aix/api/ApiRestApplication.java
@@ -1,14 +1,28 @@
 package com.aix.api;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class ApiRestApplication {
+	private static final Logger logger = LogManager.getLogger(ApiRestApplication.class);
 	public static void main(String[] args) {
+		logger.info("Info log");
+		ThreadContext.put("Prueba", "123456789");
+		logger.info("Info log2");
 		SpringApplication.run(ApiRestApplication.class, args);
 	}
-
+	public void run(ApplicationArguments applicationArguments) throws Exception {
+        logger.debug("Debugging log");
+        logger.info("Info log");
+        logger.warn("Hey, This is a warning!");
+        logger.error("Oops! We have an Error. OK");
+        logger.fatal("Damn! Fatal error. Please fix me.");
+    }
 
 }
--- src/main/java/com/aix/api/controller/MovilController.java
+++ src/main/java/com/aix/api/controller/MovilController.java
@@ -2,7 +2,11 @@ package com.aix.api.controller;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,14 +21,18 @@ import com.aix.api.dao.entity.MovilEntity;
 @RequestMapping("/api")
 public class MovilController {
 
-
+	private static final Logger logger = LogManager.getLogger(MovilController.class);
 	@GetMapping("/movil")
 	public ResponseEntity<?> getAllMovil(){
 		Map<String, Object> response = new HashMap<String, Object>();
-		System.out.print("Pasa request");
 		MovilEntity moviles = new MovilEntity();
 		response.put("moviles", moviles);
-		System.out.print("Pasa response");
+		ThreadContext.put("sessionId", UUID.randomUUID().toString());
+		ThreadContext.put("userId", "user");
+
+		//To clear context
+		//ThreadContext.clearAll();
+		logger.info(" - Prueba");
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
 	}
 	@PostMapping("/movil")
